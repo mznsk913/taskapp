63f696841cd27e4babf0ed8803da120f9c966ee5
saki.mizuno <mizunosaki9@gmail.com>
2023-10-26 13:29:32 +0900

    Initial Commit

---
+++ taskapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  taskapp
+//
+//  Created by Saki Mizuno on 2023/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
