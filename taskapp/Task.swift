80fb0d9ac337f741e22cc196746a281ae8dbac99
saki.mizuno <mizunosaki9@gmail.com>
2023-10-28 22:25:37 +0900

    テキストの内容まで完了済みこれ以降課題の内容となる

---
+++ taskapp/Task.swift
@@ -0,0 +1,23 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by Saki Mizuno on 2023/10/28.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @Persisted(primaryKey: true) var id: ObjectId
+
+    // タイトル
+    @Persisted var title = ""
+
+    // 内容
+    @Persisted var contents = ""
+
+    // 日時
+    @Persisted var date = Date()
+
+}
